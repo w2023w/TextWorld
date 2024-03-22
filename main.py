77f5cfbd0a644133bcf1a35ca7ecdea17c3782c6
王旭升 <1434405674@qq.com>
2024-03-22 17:11:03 +0800

    创建仓库

---
+++ main.py
@@ -0,0 +1,231 @@
+﻿# _*_ coding:utf-8 _*_
+
+import os
+
+os.system('')
+
+
+class Background:
+    def __init__(self, symbol="一"):
+        self.z_index = 0
+        self.symbol = symbol
+
+    def __str__(self):
+        return f"这是第{self.z_index+1}层的地板"
+        
+    def get_symbol(self):
+        return self.symbol
+
+
+class Wall:
+    def __init__(self, x, y, z=1, i=0):
+        self.id = i
+        self.z_index = z
+        self.symbol = "墙"
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"这是墙"
+
+    def get_symbol(self):
+        return self.symbol
+
+
+class Player:
+    def __init__(self, x, y, z=1):
+        self.z_index = 1
+        self.symbol = "我"
+        self.x = x
+        self.y = y
+
+    def get_symbol(self):
+        return self.symbol
+
+
+class  Scene:
+    def __init__(self, name, w, h):
+        self.id = name
+        self.width = w
+        self.height = h
+        self.txtmap = {0:[]}
+        self.rendered = ''
+        self.objs = {}
+        self.init_bg()
+
+    def __str__(self):
+        return f"这是一个名为 {self.name} 的场景"
+
+    def init_bg(self, bg=Background()):
+        for m in range(self.height):
+            line = ""
+            for n in range(self.width):
+                line += bg.symbol
+            self.txtmap[bg.z_index].append(line)
+        self.rendering()
+        
+    def rendering(self):
+        for line in self.txtmap[0]:
+            self.rendered.append(line+'\n')
+
+    def show(self):
+        print(self.rendered)
+
+    def put_obj(self, name, x, y):
+        obj = globals()[name](x=x, y=y)
+        try:
+            a = len(self.txtmap[obj.z_index])
+        except:
+            self.txtmap[obj.z_index] = {}
+        self.txtmap[obj.z_index][obj.x,obj.y] = obj.symbol
+        
+        try:
+            self.objs[name].append(obj)
+        except:
+            self.objs[name] = []
+            self.objs[name].append(obj)
+
+    def update_scence(self, name, x, y):
+        self.put_obj(name, x, y)
+
+    def scence_editor(self):
+        #os.system('cls')
+        tolog(f"\n你进入了场景{self.name}。")
+        self.show()
+        while True:
+            cmds = input(f"|>神>世界:{self.name}）").strip().split()
+            #cls()
+            cmd = cmds[0]
+            argvs = cmds[1:]
+            tolog(f'{cmds}', ty='输入')
+            match cmd:
+                case 'add':
+                    self.add_scence()
+                case 'q':
+                    tolog(f"你离开了世界{self.name}！")
+                    break
+                case 'cls':
+                    os.system(cmd)
+                case __:
+                    tolog('指令错误；\nq：回归神国\ncls：清屏\nadd：创建场景')
+
+
+class World:
+    def __init__(self, name):
+        self.name = name
+        self.scenes = {}
+
+    def __str__(self):
+        return f"这是一个名为 {self.name} 的世界"
+
+    def show_scenes(self):
+        try:
+            for key, val in self.scenes.items():
+                print(f'\t{key}：{val}')
+        except:
+            tolog("天地初生，这里空荡荡的")
+
+    def door2scene(self, name):
+        try:
+            self.scenes[name].scenes_editor()
+        except:
+            tolog(f"场景{name}不存在或崩溃了", ty="警告")
+            self.show_scenes()
+
+    def creat_scene(self, w=30, h=30):
+        i = str(len(self.scenes))
+        self.scenes[i]  = Scene(i, w, h)
+        globals()['tolog'](f"神创造了一个名为 {i} 场景！")
+
+    def world_editor(self):
+        #os.system('cls')
+        tolog(f"\n你进入了名为{self.name}的世界。")
+        self.show_scenes()
+        while True:
+            cmds = input(f"|>神>世界:{self.name}）").strip().split()
+            #cls()
+            cmd = cmds[0]
+            argvs = cmds[1:]
+            tolog(f'{cmds}', ty='输入')
+            match cmd:
+                case 'c':
+                    self.creat_scene()
+                case 'q':
+                    tolog(f"你离开了世界{self.name}！")
+                    break
+                case 'cls':
+                    os.system(cmd)
+                case 'door':
+                    self.door2scence(argvs[0])
+                case __:
+                    tolog('指令错误；\nq：回归神国\ncls：清屏\nadd：创建场景')
+
+
+
+class God:
+    def __init__(self,name='god'):
+        self.name = name
+        self.worlds = {}
+
+    def creat_world(self):
+        i = str(len(self.worlds))
+        self.worlds[i]  = World(i)
+        globals()['tolog'](f"尊贵的创世神{self.name}冕下创造了一个名为 {i} 世界！")
+
+    def show_worlds(self):
+        try:
+            for key, val in self.worlds.items():
+                print(f'\t{key}：{val}')
+        except:
+            tolog("天地未分，世界不存")
+
+    def door2world(self, name):
+        try:
+            self.worlds[name].world_editor()
+        except:
+            tolog(f"世界{name}不存在或崩溃了", ty="警告")
+            self.show_worlds()
+
+    def start_working(self):
+        #os.system('cls')
+        tolog(f"\n欢迎尊贵的{self.name}冕下归来，文字世界有你更精彩！")
+        while True:
+            cmds = input(f"|>神:{self.name}）").strip().split()
+            #cls()
+            cmd = cmds[0]
+            argvs = cmds[1:]
+            tolog(f'{cmds}', ty="输入")
+            match cmd:
+                case 'c':
+                    self.creat_world()
+                case 'q':
+                    tolog(f"恭送{self.name}冕下,文字世界期待你的再次青睐！")
+                    break
+                case 'sws':
+                    self.show_worlds()
+                case 'cls':
+                    os.system(cmd)
+                case 'door':
+                    self.door2world(argvs[0])
+                case __:
+                    tolog('指令错误；\n\033[2Cq：回归神国\ncls：清屏\n\033[2Cc：创建世界')
+
+
+def tolog(msg, ty=''):
+        print(f"{ty}\t{msg}")
+
+def cls():
+    print('\033[1A\033[0J')
+
+
+if __name__ == "__main__":
+    god_name = input('冕下尊姓大名？\n>)').strip()
+    #cls()
+    god = God(god_name)
+    god.start_working()
+
+    # print('\033[15A', end='')  # cursor up 5 lines
+    # print('\033[30C' + '我', end='')
+    # print('\033[15B', end='') 
+    # print('\r', end='')  	# cursor back to start
+    # print('\033[0J', end='')  # erase from cursor to end
---
+++ worldRule/textUI.py
@@ -0,0 +1,202 @@
+# _*_ coding:utf-8 _*_
+
+import os, time, math
+
+class Window:
+	"""文字图形界面模块"""
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.prerender_set = []
+		self.rendered = []
+		
+		self.init_render()
+		
+	def __str__(self): 
+		"""编制默认信息"""
+		return f"""
+    规格：{self.width}×{self.height}
+    样式：
+        背景：{self.base_symbol}
+        边型：{self.sides_symbol}
+		"""
+	def init_render(self): 
+		"""初始化"""
+		self.add({'type': 'floor', 'id': '00', 'z': 0, 'symbol': '·'})
+		self.add({'type': 'reack', 'id': '01', 'x':0, 'y': 0, 'z': 0, 'width': self.width, 'height': self.height, 'fill': False, 'symbol': {'top': "—", 'right': '丨', 'bottom': '—', 'left': '丨', 'fill':'一'}})
+
+		self.render()
+
+
+	def reset_size(size, width=40, height=30): 
+		"""重置窗口大小"""
+		self.width = width
+		self.height = height
+		self.show()
+
+	def add(self, obj):  
+		"""添加图层及对象"""
+		z_index = obj['z']
+		if z_index > len(self.prerender_set) - 1:
+			delta = z_index - len(self.prerender_set) + 1
+			for i in range(delta):
+				self.prerender_set.append([])
+		self.prerender_set[z_index].append(obj)
+
+	def clear(self, oid, mod = 1):
+		"""清除指定对象或图层"""
+		if mod == 0:
+			i = int(oid)
+			if i > len(self.prerender_set) - 1:
+				return
+			self.prerender_set[i] = []
+			self.show()
+		elif mod == 1:
+			i, j = list(oid)
+			i = int(i)
+			j = int(j)
+			self.prerender_set[i].pop(j)
+			self.show()
+
+
+	def render_floor(self, symbol): 
+		"""渲染基底背景"""
+		for m in range(self.height):
+			line =  symbol * self.width
+			self.rendered.append(line)
+
+	def render_reack(self, x, y, width, height, borders, fill=False): 
+		"""渲染矩形"""
+		if x < 0:
+			x = 0
+		if y < 0:
+			y = 0
+		if x >= self.width:
+			x = self.width - width - 1
+		if y >= self.height:
+			y = self.height - height - 1
+			
+		if width + x > self.width:
+			width = self.width - x
+		if height + y > self.height:
+			height = self.height - y
+		start_h = x
+		end_h = x + width - 1
+		start_v = y
+		end_v = y + height - 1
+		
+		self.render_line(borders['top'], x, y, width, 0)
+		self.render_line(borders['bottom'], x, y+height-1, width, 0)
+		self.render_line(borders['left'], x, y, height, None)
+		self.render_line(borders['right'], x+width-1, y, height, None)
+
+		if fill:
+			for m in range(height-2):
+				self.render_fill(x+1, m+y+1, width-2, borders['fill'])
+
+	def render_fill(self, x, y, long, symbol): 
+		"""填充渲染"""
+		self.render_line(symbol, x, y, long, 0)
+
+	def render_point(self, x, y, symbol): 
+		"""渲染点"""
+		if x < 0:
+			x = 0
+		if y < 0:
+			y = 0
+		if x >= self.width:
+			x = self.width - 1
+		if y >= self.height:
+			y = self.height - 1
+		line = self.rendered[y]
+		self.rendered[y] = f"{line[:x]}{symbol}{line[x+1:]}"
+
+	def render_line(self, symbol, x, y, long, k): 
+		"""渲染直线"""
+		match k:
+			case 0:
+				'''水平直线'''
+				if x + long > self.width:
+					long = self.width - x
+				string =self.rendered[y]
+				self.rendered[y] = f'{string[:x]}' + f'{symbol}'*(long) + f'{string[x+long:]}'
+			case None:
+				'''垂直线'''
+				if y + long > self.height:
+					long = self.height - y
+				for i in range(long):
+					string =self.rendered[y+i]
+					self.rendered[y+i] = f"{string[:x]}{symbol}{string[x+1:]}"
+			case __:
+				'''任意非水平非垂直的直线'''
+				a = math.atan(k)
+				delta_x = int(long * math.cos(a))
+				for x2 in range(delta_x):
+					self.render_point(x+x2, y+int(x2*k), symbol)
+
+	def render(self, z_index=0): 
+		"""全渲染成字符串列表"""
+		if z_index > len(self.prerender_set) or z_index == 0:
+			z_index=len(self.prerender_set)
+			
+		self.rendered = []
+		index = z_index
+		for i in range(index):
+			index2 = len(self.prerender_set[i])
+			for j in range(index2):
+				p = self.prerender_set[i][j]
+				match p['type']:
+					case 'floor':
+						self.render_floor(p['symbol'])
+					case 'reack':
+						self.render_reack(p['x'], p['y'], p['width'], p['height'], p['symbol'], p['fill'])
+					case 'point':
+						self.render_point(p['x'], p['y'], p['symbol'])
+					case 'line':
+						self.render_line(p['symbol'], p['x'], p['y'], p['l'], p['k'])
+		self.show()
+
+	def show(self): 
+		"""展示渲染结果"""
+		os.system('cls')
+		for line in self.rendered:
+			print('\t' + line)
+
+
+if __name__ == "__main__":
+	width = 40
+	height = 30
+	window = Window(width, height)
+
+	window.add({'type': 'reack', 'id': '10', 'x':12, 'y': 10, 'z': 1, 'width': 25, 'height': 15, 'fill': True, 'symbol': {'top': "墙", 'right': '墙', 'bottom': '墙', 'left': '墙', 'fill': '艹'}})
+	time.sleep(1)
+	window.render()
+	window.add({'type': 'line', 'id': '11', 'x':5, 'y': 5, 'z': 1, 'l': 20, 'k': None, 'symbol': '纵'})
+	time.sleep(1)
+	window.render()
+	window.add({'type': 'line', 'id': '12', 'x':5, 'y': 5, 'z': 1, 'l': 30, 'k': 0, 'symbol': '横'})
+	time.sleep(1)
+	window.render()
+	window.add({'type': 'line', 'id': '12', 'x':5, 'y': 5, 'z': 1, 'l': 30, 'k': 1, 'symbol': '线'})
+	time.sleep(1)
+	window.render()
+	window.add({'type': 'point', 'id': '14', 'x':19, 'y': 14, 'z': 1, 'symbol': '我'})
+	time.sleep(1)
+	window.render()
+	window.clear('10')
+	time.sleep(1)
+	window.render()
+	
+	x0 = 19
+	y0 = 14
+	r = 12
+	s = 17
+	for t in range(s):
+		rad = (2*math.pi/s)*t + 0*math.pi
+		x = math.cos(rad) * r
+		y = math.sin(rad) * r
+		window.add({'type': 'point', 'id': f'{13+t}', 'x': x0+int(x), 'y': y0+int(y), 'z': 1, 'symbol': '〇'})
+		window.render()
+	os.system('pause')
+		
+		
